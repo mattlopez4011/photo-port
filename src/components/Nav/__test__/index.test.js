1586759b2a1de7e02b2162c7fe0ab428f8273c08
mattlopez4011 <matthew.lopez4011@gmail.com>
2022-03-14 14:47:11 -0500

    create about nav test

---
+++ src/components/Nav/__test__/index.test.js
@@ -0,0 +1,35 @@
+// __tests__/Nav.test.js with hard coded categories
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
+afterEach(cleanup);
+describe("Nav component", () => {
+    // baseline test
+    it("renders", () => {
+        render(<Nav />);
+    });
+    // snapshot test
+    it("matches snapshot", () => {
+        const { asFragment } = render(<Nav />);
+        // assert value comparison
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
+describe("emoji is visible", () => {
+    it("inserts emoji into the h2", () => {
+        // Arrange
+        const { getByLabelText } = render(<Nav />);
+        // Assert
+        expect(getByLabelText("camera")).toHaveTextContent(":camera_with_flash:");
+    });
+});
+describe("links are visible", () => {
+    it("inserts text into the links", () => {
+        //   Arrange
+        const { getByTestId } = render(<Nav />);
+        // Asssert
+        expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
+        expect(getByTestId("about")).toHaveTextContent("About me");
+    });
+});
